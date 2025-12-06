269bd1ddd6a329086550e25678edad246c49e94d
drajan <anastasia.fed2009@gmail.com>
2025-12-06 20:19:29 +0200

    Refactor Character class with new abilities and methods

--- Character/Character.js
+++ Character/Character.js
@@ -1,15 +1,26 @@
 import {BaseEntity} from "../BaseEntity";
 import {Inventory} from "../Inventory/Inventory";
 
+
 export class Character extends BaseEntity {
     #hp;
-    #maxHp;
 
-    constructor(name, maxHp, baseDamage) {
+    constructor(name, hp, baseDMG) {
         super(name);
-        this.#hp = maxHp;
-        this.#maxHp = maxHp;
-        this.baseDamage = baseDamage;
+        this.#hp = hp;
+        this.maxCritRate = 1;
+        this.maxCritDMG = 2;
+        //attack
+        this.critRate = 0.5;
+        this.critDMG = 1.5;
+        this.baseDMG = baseDMG;
+        //support
+        this.critRateSupportEffect = 0;
+        this.critDMGSupportEffect = 0;
+        //heal
+        this.healEffect = 0;
+        //hit rate for long distance chars
+        this.effectHITRate = 0.5;
         this.inventory = new Inventory(this);
     }
 
@@ -26,7 +37,7 @@ export class Character extends BaseEntity {
             return;
         }
 
-        this.#hp = Math.min(this.#hp + amount, this.#maxHp);
+        this.#hp = Math.min(this.#hp + amount);
     }
 
     takeDamage(amount) {
@@ -41,12 +52,74 @@ export class Character extends BaseEntity {
         }
     }
 
-    attack(target) {
+    takeHeal(amount) {
         if (!this.isAlive) {
             return;
         }
 
-        const damage = this.baseDamage;
+        this.#hp += amount;
+    }
+
+    takeSupport(amountCrit, amountDMG) {
+        if (!this.isAlive) {
+            return;
+        }
+
+        this.critRate = Math.min(this.critRate + amountCrit, this.maxCritRate);
+        this.critDMG = Math.min(this.critDMG + amountDMG, this.maxCritDMG);
+    }
+
+    attack(target) {
+        if (!this.isAlive) {
+            return;
+        }
+        const isCrit = Math.random() <= this.critRate;
+        let damage;
+
+        if(isCrit) {
+            damage = this.baseDMG * this.critDMG
+        } else {
+            damage = this.baseDMG;
+        }
+
+        console.log(`${this.displayName} attack ${target.displayName} and did ${damage} damage.`);
+        target.takeDamage(damage);
+    }
+
+    healing(target) {
+        if (!this.isAlive) {
+            return;
+        }
+
+        const healNum = target.hp * this.healEffect;
+        console.log(`${this.displayName} healed ${target.displayName} added ${healNum} hp.`);
+        target.takeHeal(healNum);
+
+    }
+
+    support(target) {
+        if (!this.isAlive) {
+            return;
+        }
+        const critRateNum = this.critRateSupportEffect;
+        const critDMGNum = this.critDMGSupportEffect;
+        console.log(`${this.displayName} supported ${target.displayName} added ${critRateNum} crit rate and ${critDMGNum} crat DMG.`);
+        target.takeSupport(critRateNum, critDMGNum);
+    }
+
+    distanceAttack(target) {
+        if (!this.isAlive) {
+            return;
+        }
+
+        const isHIT = Math.random() <= this.effectHITRate;
+        let damage;
+
+        if(isHIT) {
+            damage = this.baseDMG;
+        } else {
+            damage = 0;
+        }
 
         console.log(`${this.displayName} attack ${target.displayName} and did ${damage} damage.`);
         target.takeDamage(damage);
